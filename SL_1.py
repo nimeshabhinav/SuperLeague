df92ab3535b996e9dcdbfca9506ac9a8db137234
nimeshabhinav <nimesh.abhinav@gmail.com>
2021-04-18 20:50:50 +0530

    4th version of change

--- SL_1.py
+++ SL_1.py
@@ -2,4 +2,4 @@ Superleague=input("Are you part of SuperLeague?")
 if Superleague=="yes":
     print("Complete the assignment on time")
 else:
-    print("Get Help from SMEs");
+    print("Keep doing");
